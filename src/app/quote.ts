94a0c658cf05cdc1317254998dc5bdb2ce6a13e4
trevor-isiche <isiche.trevor@gmail.com>
2019-12-01 14:16:26 +0300

    add constructor for quote

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,13 @@
 export class Quote {
-    // public showDescription: boolean;
-    constructor(public id:number, public name:string,public description:string,public completeDate:Date,public contributor:string){
-        // this.showDescription = false
+
+    showDescription= false;
+    
+      constructor(
+        public id: number,
+        public name: string,
+        public description: string,
+        public submitter: string,
+        public completeDate: Date) {
     }
-}
+      }
+    //contains properties of all inputs
