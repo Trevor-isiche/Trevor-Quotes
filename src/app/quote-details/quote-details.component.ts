6584cb74a71bf0b8d06f0234ce684ce32f8b1af3
trevor-isiche <isiche.trevor@gmail.com>
2019-12-03 10:25:43 +0300

    add export class function

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -7,8 +7,23 @@ import { Quote } from '../quote';
   styleUrls: ['./quote-details.component.css']
 })
 export class QuoteDetailsComponent implements OnInit {
+  upvotes:number = -1;
+  downvotes:number = -1;
+  showUpvotes:string= "";
+  showDownVotes:string="";
+
 
   @Input() quote:Quote;
+
+  voteUp(){
+    this.upvotes = this.upvotes+1;
+    this.showUpvotes = "" + this.upvotes;
+  }
+
+  voteDown(){
+    this.downvotes = this.downvotes+1;
+    this.showDownVotes = ""+this.downvotes;
+  }
 
   constructor() { }
 
