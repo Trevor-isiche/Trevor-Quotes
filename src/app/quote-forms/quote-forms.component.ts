5ca4939ba75fa251aeff9d04107d063d16ce3286
trevor-isiche <isiche.trevor@gmail.com>
2019-12-03 10:29:38 +0300

    add export class

--- src/app/quote-forms/quote-forms.component.ts
+++ src/app/quote-forms/quote-forms.component.ts
@@ -1,11 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit,Output,EventEmitter } from '@angular/core';
+import {Quote} from '../quote';
 @Component({
   selector: 'app-quote-forms',
   templateUrl: './quote-forms.component.html',
   styleUrls: ['./quote-forms.component.css']
 })
 export class QuoteFormsComponent implements OnInit {
+  quote:string;
+  submittedBy:string;
+  author:string;
+  id:number;
+  date:Date;
+  anyType:any;
+  @Output() newQuote = new EventEmitter<Quote>();
+ 
+  add(){
+    this.anyType = new Quote(this.id,this.author,this.quote,this.submittedBy,this.date);
+    this.newQuote.emit(this.anyType);
+  }
+
+  outPutQuote:any;
+
 
   constructor() { }
 
